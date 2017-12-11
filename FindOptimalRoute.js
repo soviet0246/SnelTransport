dae049cd4fad870a41e92ac3f770907ded3ea27d
ishant3 <upadhyay.ishant@yahoo.com>
2017-12-11 10:53:53 +0100

    google distance Matrix

---
+++ FindOptimalRoute.js
@@ -0,0 +1,73 @@
+﻿
+var originlist = [];
+var destinationlist = [];
+
+$(document).ready(function () {
+
+    $("#btnDeliveryAddress").click(function () {
+
+        $.ajax({
+
+            url: "http://localhost:64504/Service.svc/rest/GetCustomer",
+            type: "GET",
+            Accept: "application/json",
+            success: function (resultdata) {
+               // console.log(resultdata);
+                $.each(resultdata, function (k, v) {
+
+                   // var id = v.Id;
+                    var name = v.Name;
+                    var street = v.Street;
+                    var houseNumber = v.HouseNumber;
+                    var postcode = v.PostCode;
+                    var city = v.City;
+                    var address = street+" " + houseNumber+", " + postcode+", " + city;
+                   // var telephone = v.Telephone;
+                   // var fax = v.Fax;
+
+                    $("#tblGetAddress").append("<tr><td>" + name + "</td><td>" + street + " " + houseNumber + ", " + postcode + ", " + city + "</td></tr>")
+
+                    //var address = street + " " + houseNumber + ", " + postcode + ", " + city;
+
+                    
+                   
+
+                });
+
+               // var deliverlist_data = [];
+                // deliverlist_data = resultdata
+                for (var i = 0; i < resultdata.length; i++) {
+                   // console.log(resultdata[i].city);
+                    originlist[i] = resultdata[i].Street + " " + resultdata[i].HouseNumber + ", " + resultdata[i].PostCode + ", " + resultdata[i].City;
+                    destinationlist[i] = originlist[i] ;
+                }
+
+                console.log(originlist);
+                console.log(destinationlist);
+
+
+                for (var i = 0; i < originlist.length; i++)
+                    for (var j = i; j < destinationlist.length; j++) {
+                        console.log("Origin Address: " + originlist[i] + "  and Destination Address:" + destinationlist[j]);
+                    }
+                
+            },
+            error: function (e) { 
+                alert("something went wrong!");
+            }
+
+
+            
+
+        });
+    });
+
+
+
+   
+
+
+   
+
+
+});
---
+++ distanceMatrix.js
@@ -0,0 +1,41 @@
+﻿function initMap() {
+
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+        {
+            origins: ['Amsterdam', 'Venlo', 'Utretch', 'Gouda'],
+            destinations: ['Amsterdam', 'Venlo', 'Utretch', 'Gouda'],
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false,
+        }, callback);
+
+    function callback(response, status) {
+        // See Parsing the Results for
+        // the basics of a callback function.
+        if (status == 'OK') {
+            var origins = response.originAddresses;
+            var destinations = response.destinationAddresses;
+
+            for (var i = 0; i < origins.length; i++) {
+                var results = response.rows[i].elements;
+
+                for (var j = 0; j < results.length; j++) {
+                    var element = results[j];
+                    var distance = element.distance.text;
+                    var duration = element.duration.text;
+                    var from = origins[i];
+                    var to = destinations[j];
+
+                    console.log("Total Distance: " + distance + ", Total Durations: " + duration + ", From: " + from + " to " + to);
+                }
+
+
+            }
+
+        }
+    }
+
+
+}
